0e78a86308a89aa52754571e06a0e0118f7f06ea
Calixto Rodrigues Macedo <calixto.macedo@gmail.com>
2025-01-09 07:16:41 -0300

    List maps

--- java-lambda-learning/src/main/java/bv/com/cmdev/FiltersLambda.java
+++ java-lambda-learning/src/main/java/bv/com/cmdev/FiltersLambda.java
@@ -1,5 +1,7 @@
 package bv.com.cmdev;
 
+import bv.com.cmdev.domain.Person;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,9 +18,20 @@ public class FiltersLambda {
         }
         System.out.println("Linguagens com mais de 3 letras sem lambda"+ languageFiltered);
 
-
         List<String> languageFilteredLambda = languages.stream().filter(language -> language.length() > 3).toList();
         System.out.println("Linguagens com mais de 3 letras com lambda"+ languageFilteredLambda);
+
+        List<Person> personList = Arrays.asList(
+                new Person(1, "Calixto", "Caio", "Rodrigues", "Macedo", 52),
+                new Person(2, "Rosimeire", "Rosy", "Santos", "Cunegundes", 37),
+                new Person(3, "Adrielly Thamyres", "Tatá", "Pimentel", "Macedo", 22),
+                new Person(4, "Davi Lucca", "Lukinha", "Cunegundes", "Macedo", 2)
+        );
+
+        List<String> nomes = personList.stream().map( person -> person.getName()).toList();
+        List<Integer> idades = personList.stream().map(Person::getAge).toList();
+
+        System.out.println("Lista de nomes: "+ nomes +" e idades: "+ idades);
 
     }
 }
